69c3d130ea11307f128b2466359973ba0bb6d315
kbpatel <kbpatel3@catamount.wcu.edu>
2024-06-11 11:35:10 -0400

    Added documentation for the interfaces.

--- src/repository/ItemRepository.java
+++ src/repository/ItemRepository.java
@@ -5,9 +5,34 @@ import model.Item;
 import java.util.List;
 
 public interface ItemRepository {
+    /**
+     * Save item to database
+     * @param item item to save
+     */
     void save(Item item);
+
+    /**
+     * Delete item from database
+     * @param barcode barcode of item to delete
+     */
     void delete(String barcode);
+
+    /**
+     * Update item in database
+     * @param item item to update
+     */
     void update(Item item);
+
+    /**
+     * Find item by barcode
+     * @param barcode barcode of item to find
+     * @return item with the barcode
+     */
     Item findByBarcode(String barcode);
+
+    /**
+     * Find all items in database
+     * @return list of all items
+     */
     List<Item> findAll();
 }
--- src/repository/TransactionRepository.java
+++ src/repository/TransactionRepository.java
@@ -6,6 +6,17 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionRepository {
+    /**
+     * Save a transaction to the DB
+     * @param transaction the transaction to save
+     */
     void save(Transaction transaction);
+
+    /**
+     * Retrieve transactions from the DB within a date range
+     * @param startDate the start date
+     * @param endDate the end date
+     * @return a list of transactions within the date range
+     */
     List<Transaction> findByDateRange(LocalDate startDate, LocalDate endDate);
 }
