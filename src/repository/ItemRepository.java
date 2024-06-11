0371aad4b86a65bbd13ef04e8ccc729b88c1df6b
kbpatel <kbpatel3@catamount.wcu.edu>
2024-06-11 10:58:02 -0400

    Interfaces for item and transaction management implemented.

--- src/repository/ItemRepository.java
+++ src/repository/ItemRepository.java
@@ -1,4 +1,13 @@
 package repository;
 
-public class ItemRepository {
+import model.Item;
+
+import java.util.List;
+
+public interface ItemRepository {
+    void save(Item item);
+    void delete(String barcode);
+    void update(Item item);
+    Item findByBarcode(String barcode);
+    List<Item> findAll();
 }
--- src/repository/TransactionRepository.java
+++ src/repository/TransactionRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
-public class TransactionRepository {
+import model.Transaction;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface TransactionRepository {
+    void save(Transaction transaction);
+    List<Transaction> findByDateRange(LocalDate startDate, LocalDate endDate);
 }
