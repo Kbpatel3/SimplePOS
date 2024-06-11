b8fadcd0165d3157ae3f2a553d16539ba61bf9fe
kbpatel <kbpatel3@catamount.wcu.edu>
2024-06-11 10:32:37 -0400

    SceneManager for the JavaFX application.

--- src/util/SceneManager.java
+++ src/util/SceneManager.java
@@ -1,4 +1,43 @@
 package util;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SceneManager {
+    /** Static reference to the primary stage */
+    private static Stage stage;
+
+    /** Static reference to the scenes */
+    private static Map<String, Scene> scenes = new HashMap<>();
+
+    /**
+     * Set the primary stage
+     * @param primaryStage the primary stage
+     */
+    public static void setStage(Stage primaryStage) {
+        stage = primaryStage;
+    }
+
+    /**
+     * Add a scene to the scene map
+     * @param name the name of the scene
+     * @param scene the scene reference
+     */
+    public static void addScene(String name, Scene scene) {
+        scenes.put(name, scene);
+    }
+
+    /**
+     * Switch to a scene by name
+     * @param name the name of the scene
+     */
+    public static void switchScene(String name) {
+        if (scenes.containsKey(name)) {
+            stage.setScene(scenes.get(name));
+            stage.show();
+        }
+    }
 }
